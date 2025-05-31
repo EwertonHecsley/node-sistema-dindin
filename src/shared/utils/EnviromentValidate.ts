5b9336cd96d90812d15942a6c78d2323873400e5
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-05-31 15:54:00 -0300

    feat: instalando zod para validacao de variavies

---
+++ src/shared/utils/EnviromentValidate.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+export default class EviromentValidator {
+    private readonly envSchema = z.object({
+        NODE_ENV: z.enum(['development', 'production', 'test']),
+        PORT: z.string().regex(/^\d+$/).transform(Number),
+        DATABASE_URL: z.string().url()
+    }).passthrough();
+
+
+    validateEnviromentVariables(): void {
+        const result = this.envSchema.safeParse(process.env);
+
+        if (!result.success) {
+            console.log(result.error.format());
+            process.exit(1);
+        }
+
+        const env = result.data;
+
+        switch (env.NODE_ENV) {
+            case 'development':
+                console.log('Environment is set to development.');
+                break;
+            case 'production':
+                console.log('Environment is set to production.');
+                break;
+            case 'test':
+                console.log('Environment is set to test.');
+                break;
+        }
+
+        console.log('Validating environment variables');
+    }
+}
