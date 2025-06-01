59e7bba2d843b1d58fb864ef3dee4c1168e06309
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-01 12:16:59 -0300

    feat: construcao rotas da entidade usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import fastify from 'fastify';
+import { UserPrismaRepository } from './infra/database/repository/user/UserPrismaRepository';
+import { UserController } from './infra/http/controllers/user/UserController';
+import { UserRoutes } from './infra/http/routes/user/UserRoutes';
 
 const app = fastify();
+
+const userRepository = new UserPrismaRepository()
+const userController = new UserController(userRepository);
+const userRoutes = new UserRoutes(userController);
+
+userRoutes.register(app);
 
 export default app;
---
+++ src/infra/http/routes/user/UserRoutes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+import { UserController } from "../../controllers/user/UserController";
+import { validateBody } from "../../middlewares/validate";
+import { schemaUserDto } from "../../controllers/user/dto/schemaUserDto";
+
+export class UserRoutes {
+    constructor(private readonly controller: UserController) { }
+
+    async register(app: FastifyInstance) {
+        app.post('/user/v1', {
+            preHandler: validateBody(schemaUserDto),
+            handler: this.controller.store.bind(this.controller)
+        });
+    }
+}
