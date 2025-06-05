dd5419ff663b3f1dcc054ebd853a779cd27c4727
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-05 15:02:40 -0300

    feat: construcao e finalizacao controller para criar uma nova transacao

---
+++ src/infra/database/prisma/mapper/TransactionPrismaMaper.ts
@@ -0,0 +1,28 @@
+import { Transaction } from '@/core/domain/transaction/entity/Transition';
+import { Transaction as TransactionDatabase } from '../../../../../generated/prisma';
+
+export class TransactionPrismaMapper {
+  static toDatabase(entity: Transaction): TransactionDatabase {
+    return {
+      id: entity.getValueId().getValueId(),
+      description: entity.description,
+      value: entity.value,
+      date: entity.date,
+      type: entity.type,
+      category_id: entity.category_id,
+      user_id: entity.user_id,
+      createdAt: entity.createdAt,
+    };
+  }
+
+  static toDomain(entity: TransactionDatabase): Transaction {
+    return Transaction.create({
+      description: entity.description,
+      value: entity.value,
+      date: entity.date,
+      type: entity.type,
+      category_id: entity.category_id,
+      user_id: entity.user_id,
+    });
+  }
+}
---
+++ src/infra/http/controllers/transaction/TransactionController.ts
@@ -0,0 +1,38 @@
+import { CategoryRepository } from "@/core/domain/category/repository/CategoryRepository";
+import { TransactionRepository } from "@/core/domain/transaction/repository/TransictionRepository";
+import { CreateTransactionUseCase } from "@/core/domain/transaction/use-case/Create";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateTransactionDto } from "./dto/schemaTransactionDto";
+import { getUserIdOrThrow } from "@/shared/utils/getUserIdOrThrow";
+import { logger } from "@/shared/utils/logger";
+
+export class TransactionController {
+    private readonly create: CreateTransactionUseCase;
+
+    constructor(
+        private readonly categoryRepository: CategoryRepository,
+        private readonly transactionRepository: TransactionRepository
+    ) {
+        this.create = new CreateTransactionUseCase(this.transactionRepository, this.categoryRepository);
+    }
+
+    async store(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+        const props = request.body as CreateTransactionDto;
+        const user_id = getUserIdOrThrow(request, reply);
+        if (!user_id) return;
+
+        const result = await this.create.execute({ ...props, user_id });
+        if (result.isLeft()) {
+            logger.error('Erro create transaction.');
+            const error = result.value;
+            reply.status(error.statusCode).send({ message: error.message });
+            return;
+        }
+
+        reply.status(201).send({
+            message: 'Created transaction sucessfully.',
+            transaction: result.value
+        });
+        logger.info('Created transaction sucessfully.');
+    }
+}
