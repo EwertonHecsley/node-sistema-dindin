efbcfebc5782592639edfef32c1969f578e31ca1
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-06 12:12:45 -0300

    feat: controller atulaizar transacao

--- src/infra/http/controllers/transaction/TransactionController.ts
+++ src/infra/http/controllers/transaction/TransactionController.ts
@@ -11,11 +11,14 @@ import { UserRepository } from '@/core/domain/user/repository/UserRepository';
 import { NotFound } from '@/shared/errors/custom/NorFound';
 import { FindTransactionUseCase } from '@/core/domain/transaction/use-case/Find';
 import { schemaTransactionParamsDto } from './dto/schemaParamsTransactionDto';
+import { UpdateTransactionUseCase } from '@/core/domain/transaction/use-case/Update';
+import { UpdateTransactionDto } from './dto/schemaTransactionUpdateDto';
 
 export class TransactionController {
   private readonly create: CreateTransactionUseCase;
   private readonly listAll: ListAllTransactions;
   private readonly find: FindTransactionUseCase;
+  private readonly save: UpdateTransactionUseCase;
 
   constructor(
     private readonly categoryRepository: CategoryRepository,
@@ -33,6 +36,11 @@ export class TransactionController {
       this.userRepository,
       this.categoryRepository,
     );
+    this.save = new UpdateTransactionUseCase(
+      this.transactionRepository,
+      this.userRepository,
+      this.categoryRepository,
+    )
   }
 
   async store(request: FastifyRequest, reply: FastifyReply): Promise<void> {
@@ -99,5 +107,37 @@ export class TransactionController {
       transaction: TransactionPresenter.toHTTP(result.value.transaction, result.value.category),
     });
     logger.info('Found transaction sucessfully.');
+  }
+
+  async update(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const user_id = getUserIdOrThrow(request, reply);
+    if (!user_id) return;
+
+    const paramsValidate = schemaTransactionParamsDto.safeParse(request.params);
+    if (!paramsValidate.success) {
+      reply.status(400).send({ message: 'Invalid params for transaction.' });
+      return;
+    }
+
+    const { id } = paramsValidate.data;
+    const props = request.body as UpdateTransactionDto;
+
+    const result = await this.save.execute({
+      id,
+      user_id,
+      ...props,
+      date: props.date ? new Date(props.date) : undefined
+    });
+    if (result.isLeft()) {
+      logger.error('Error updating transaction.');
+      const error = result.value;
+      reply.status(error.statusCode).send({ message: error.message });
+      return;
+    }
+
+    reply.status(200).send({
+      message: 'Updated transaction sucessfully.',
+    });
+    logger.info('Updated transaction sucessfully.');
   }
 }
